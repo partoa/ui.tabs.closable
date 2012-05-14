18c64669078b71d94c0118eb30369b7080ecae87
patrick atambo <>
2012-05-14 08:36:15 +0300

    Tab removal fully fixed. Source minified.

--- ui.tabs.closable.js
+++ ui.tabs.closable.js
@@ -6,56 +6,55 @@
  * 
  * http://github.com/andrewwatts/ui.tabs.closable
  */
-(function() {
+(function($) {
     
-var ui_tabs_tabify = $.ui.tabs.prototype._tabify;
+    var ui_tabs_tabify = $.ui.tabs.prototype._tabify;
 
-$.extend($.ui.tabs.prototype, {
+    $.extend($.ui.tabs.prototype, {
 
-    _tabify: function() {
-        var self = this;
+        _tabify: function() {
+            var self = this;
 
-        ui_tabs_tabify.apply(this, arguments);
+            ui_tabs_tabify.apply(this, arguments);
 
-        // if closable tabs are enable, add a close button
-        if (self.options.closable === true) {
+            // if closable tabs are enable, add a close button
+            if (self.options.closable === true) {
 
-            var unclosable_lis = this.lis.filter(function() {
-                // return the lis that do not have a close button
-                return $('span.ui-icon-circle-close', this).length === 0;
-            });
+                var unclosable_lis = this.lis.filter(function() {
+                    // return the lis that do not have a close button
+                    return $('span.ui-icon-circle-close', this).length === 0;
+                });
 
-            // append the close button and associated events
-            unclosable_lis.not('.tab-notclosable').each(function() {
-                $(this)
+                // append the close button and associated events
+                unclosable_lis.not('.tab-notclosable').each(function() {
+                    $(this)
                     .append('<a href="#"><span class="ui-icon ui-icon-circle-close"></span></a>')
                     .find('a:last')
-                        .hover(
-                            function() {
-                                $(this).css('cursor', 'pointer');
-                            },
-                            function() {
-                                $(this).css('cursor', 'default');
-                            }
+                    .hover(
+                        function() {
+                            $(this).css('cursor', 'pointer');
+                        },
+                        function() {
+                            $(this).css('cursor', 'default');
+                        }
                         )
-                        .click(function() {
-                            var index = self.lis.index($(this).parent());
-                            if (index > -1) {
-                                // call _trigger to see if remove is allowed
-                                if (false === self._trigger("closableClick", null, self._ui( $(self.lis[index]).find( "a" )[ 0 ], self.panels[index] ))) return;
+                    .click(function() {
+                        var index = self.lis.index($(this).parent());
+                        if (index > -1) {
+                            // call _trigger to see if remove is allowed
+                            if (false === self._trigger("closableClick", null, self._ui( $(self.lis[index]).find( "a" )[ 0 ], self.panels[index] ))) return;
 
-                                // remove this tab
-                                self.remove(index);
-                                $($(this).prev('a').attr('href')).remove();
-                            }
+                            // remove this tab
+                            self.remove(index);
+                            $($(this).prev('a').attr('href')).remove();
+                        }
 
-                            // don't follow the link
-                            return false;
-                        })
+                        // don't follow the link
+                        return false;
+                    })
                     .end();
-            });
+                });
+            }
         }
-    }
-});
-    
+    });
 })(jQuery);
